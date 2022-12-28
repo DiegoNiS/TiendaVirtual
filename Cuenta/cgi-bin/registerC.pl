c5b019c7cdaa4af7c4e113238fdf2b836e56e55a
owerFRANK <olopeza@unsa.edu.pe>
2022-12-28 03:18:57 -0500

    creamos la funcion para chequear el usuario

--- Cuenta/cgi-bin/registerC.pl
+++ Cuenta/cgi-bin/registerC.pl
@@ -25,3 +25,21 @@ if (defined($usuario) and defined($correo) and defined($contrasena)) {
 else {
     showRegister();
 }
+
+sub checkUsuario {
+    my $usuarioQuery = $_[0];
+
+    my $user = 'alumno';
+    my $password = 'pweb1';
+    my $dsn ='DBI:MariaDB:database=pweb1;host=192.168.1.23';
+    my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar a la base de datos");
+
+    my $sql = "SELECT usuario FROM Clientes WHERE usuario=?";
+    my $sth = $dbh->prepare($sql);
+    $sth->execute($usuarioQuery);
+    my @row = $sth->fetchrow_array;
+    $sth->finish;
+    $dbh->disconnect;
+
+    return @row;
+}

69ef2ea33fc7b75fd9fd4dcb6151303e34e398c8
owerFRANK <olopeza@unsa.edu.pe>
2022-12-28 03:20:59 -0500

    recuperamos las variables del html

--- Cuenta/cgi-bin/registerA.pl
+++ Cuenta/cgi-bin/registerA.pl
@@ -1,5 +1,14 @@
 #!/usr/bin/perl -w
+
 use strict;
 use warnings;
-use CGI;
+use CGI qw/:standard/;
 use DBI;
+use utf8;
+
+my $q = CGI->new;
+print $q->header('text/html');
+
+my $usuario = $q->param("usuario");
+my $correo = $q->param("correo");
+my $contrasena = $q->param("contrasena");

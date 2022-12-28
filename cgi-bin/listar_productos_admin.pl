196cf00ef3a9346e67143b955e83c17dd73940df
owerFRANK <olopeza@unsa.edu.pe>
2022-12-28 07:40:53 -0500

    .

--- cgi-bin/listar_productos_admin.pl
+++ cgi-bin/listar_productos_admin.pl
@@ -17,6 +17,7 @@ print "Content-type: text/html\n\n";
 print <<HTML;
 <!DOCTYPE html>
 <html lang="en">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -28,23 +29,59 @@ print <<HTML;
     <title>Interface Administradores</title>
 </head>
 <body>
-    <div class="cont">
-        <div class="contenedor_lista" id="lista">
-            <ul>
-                
+    <div class="modal-header">
+        <button class="btn btn-lg btn-default pull-right" onclick="window.location.href='../anadir_productos.html'">Añadir mas elementos</button>
+    </div>
+
+    <table border="2px" class="form-group" class="" id="lista">
+        <tr>
+            <td scope="col">CATEGORIAS</td>
+            <td scope="col">PRODUCTOS</td>
+        </tr>
+        <br>
+        <tr>
+            <td>
 HTML
 
-while (my @row = $sth->fetchrow_array()) {
+my $user = 'alumno';
+my $password = 'pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.23";
+my $dbh = DBI->connect($dsn, $user, $password);
+
+# Ejecutar una consulta SQL para obtener los elementos de la base de datos
+my $sth1 = $dbh->prepare("select distinct Categoria from Productos");
+$sth1->execute();
+
+while (my @row = $sth1->fetchrow_array()) {
   print "<li><a href='mostrar_productos_admin.pl?tipo_producto=$row[0]'>$row[0]</a></li>\n";
 }
 
 print <<HTML;           
-            </ul>
-        </div>
-        <div class="contenedor_texto" id="Menu">
-            Esta es una sección donde se mostrarán los elementos de la clasificación de productos, para que los administradores tengan 
-        </div>
+            </td>
+            
+            <td>             
+HTML
 
+my $sth2 = $dbh->prepare("select * from Productos where Categoria=?");
+$sth2->execute($tipo_producto);
+
+while (my @row = $sth2->fetchrow_array()) {
+    print "<li>@row[1]: s/. @row[2]</li>\n";
+    print "<div class='divfrm'>";
+    print "<form class='frm'".$tipo_producto."'&nombre='".@row[1]."'\">\n";
+    print "<input type='submit' class='btn' value='Delete this'>";
+    print "</form>";
+    print "<form class='frm'".$tipo_producto."'&nombre='".@row[1]."'\">\n";
+    print "<input type='submit' class='btn' value='Edit this'></form>";
+    print "</div>";
+}
+
+print <<HTML;           
+            </ul> </td> 
+        </tr>
+    </table> 
+            
+        </div>        
     </div>
 </body>
 </html>
